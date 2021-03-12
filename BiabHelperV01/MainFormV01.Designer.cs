c8e453e6e755caaf6e34ab7857139ef83e19dcb2
Mark Kamoski <mkamoski@gmail.com>
2021-03-12 11:10:55 -0500

    Update.

--- BiabHelperV01/MainFormV01.Designer.cs
+++ BiabHelperV01/MainFormV01.Designer.cs
@@ -29,12 +29,112 @@ namespace BiabHelperV01
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResetTextButton = new System.Windows.Forms.Button();
+            this.ProcessTestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MessageTextBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MessageTextBox.Location = new System.Drawing.Point(28, 496);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.ReadOnly = true;
+            this.MessageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MessageTextBox.Size = new System.Drawing.Size(920, 123);
+            this.MessageTextBox.TabIndex = 0;
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputTextBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTextBox.Location = new System.Drawing.Point(28, 42);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InputTextBox.Size = new System.Drawing.Size(920, 328);
+            this.InputTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text From Cleanup Run";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 471);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Status Message";
+            // 
+            // ResetTextButton
+            // 
+            this.ResetTextButton.AutoSize = true;
+            this.ResetTextButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ResetTextButton.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetTextButton.Location = new System.Drawing.Point(28, 393);
+            this.ResetTextButton.Name = "ResetTextButton";
+            this.ResetTextButton.Size = new System.Drawing.Size(120, 32);
+            this.ResetTextButton.TabIndex = 4;
+            this.ResetTextButton.Text = "Reset Text";
+            this.ResetTextButton.UseVisualStyleBackColor = true;
+            this.ResetTextButton.Click += new System.EventHandler(this.ResetTextButton_Click);
+            // 
+            // ProcessTestButton
+            // 
+            this.ProcessTestButton.AutoSize = true;
+            this.ProcessTestButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ProcessTestButton.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessTestButton.Location = new System.Drawing.Point(157, 393);
+            this.ProcessTestButton.Name = "ProcessTestButton";
+            this.ProcessTestButton.Size = new System.Drawing.Size(140, 32);
+            this.ProcessTestButton.TabIndex = 5;
+            this.ProcessTestButton.Text = "Process Text";
+            this.ProcessTestButton.UseVisualStyleBackColor = true;
+            this.ProcessTestButton.Click += new System.EventHandler(this.ProcessTestButton_Click);
+            // 
+            // MainFormV01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(978, 644);
+            this.Controls.Add(this.ProcessTestButton);
+            this.Controls.Add(this.ResetTextButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.MessageTextBox);
+            this.MinimumSize = new System.Drawing.Size(1000, 700);
+            this.Name = "MainFormV01";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainFormV01";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ResetTextButton;
+        private System.Windows.Forms.Button ProcessTestButton;
     }
 }
--- BiabHelperV01/MainFormV01.cs
+++ BiabHelperV01/MainFormV01.cs
@@ -2,19 +2,140 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BiabHelperV01
 {
+    /// <summary>
+    /// This is the main startup form.
+    /// </summary>
     public partial class MainFormV01 : Form
     {
+        /// <summary>
+        /// This is the main constructor.
+        /// </summary>
         public MainFormV01()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// This handles the clearing.
+        /// </summary>
+        /// <param name="sender">This is the event source.</param>
+        /// <param name="e">These are the event arguments.</param>
+        private void ResetTextButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.UpdatePageMessage(string.Empty);
+
+                //TODO.
+
+                this.UpdatePageMessage("Note MethodBase.GetCurrentMethod()='" + MethodBase.GetCurrentMethod() + "' is Ok.");
+
+            }
+            catch (Exception ex)
+            {
+                this.UpdatePageMessage(ex);
+            }
+        }
+
+        /// <summary>
+        /// This handles the processing.
+        /// </summary>
+        /// <param name="sender">This is the event source.</param>
+        /// <param name="e">These are the event arguments.</param>
+        private void ProcessTestButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.UpdatePageMessage(string.Empty);
+
+                //TODO.
+
+                this.UpdatePageMessage("Note MethodBase.GetCurrentMethod()='" + MethodBase.GetCurrentMethod() + "' is Ok.");
+
+            }
+            catch (Exception ex)
+            {
+                this.UpdatePageMessage(ex);
+            }
+        }
+
+        /// <summary>
+        /// This will set the information on the page for status.
+        /// </summary>
+        /// <param name="targetMessage">This is the value to use.</param>
+        private void UpdatePageMessage(string targetMessage)
+        {
+            //This is a special case where we NEVER want to fail so try to handle everything gracefully.
+            try
+            {
+                string myMessage = string.Empty;
+
+                if (targetMessage == null)
+                {
+                    myMessage = "(targetMessage == null)";
+                }
+                else if (targetMessage.Trim().Length <= 0)
+                {
+                    myMessage = "(targetMessage.Trim().Length <= 0)";
+                }
+                else
+                {
+                    myMessage = (targetMessage + string.Empty).Trim();
+                }
+
+                myMessage = Environment.NewLine +
+                    "DateTime.Now.ToString(\"o\")='" + DateTime.Now.ToString("o") + "'" + " | " +
+                    "myMessage='" + myMessage + "'" + " | " +
+                    string.Empty;
+
+                this.MessageTextBox.Text = myMessage + Environment.NewLine + this.MessageTextBox.Text;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                MessageBox.Show(text: ex.ToString(), caption: "Error", buttons: MessageBoxButtons.OK);
+            }
+        }
+
+        /// <summary>
+        /// This will set the information on the page for status.
+        /// </summary>
+        /// <param name="targetMessage">This is the value to use.</param>
+        private void UpdatePageMessage(Exception targetException)
+        {
+            //This is a special case where we NEVER want to fail so try to handle everything gracefully.
+            try
+            {
+                string myMessage = string.Empty;
+
+                if (targetException == null)
+                {
+                    myMessage = "(targetException == null)";
+                }
+                else
+                {
+                    myMessage = targetException.ToString().Trim();
+                }
+
+                //Call an overload.
+                this.UpdatePageMessage(myMessage);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());                
+                MessageBox.Show(text: ex.ToString(), caption: "Error", buttons: MessageBoxButtons.OK);
+            }
         }
     }
 }
+
